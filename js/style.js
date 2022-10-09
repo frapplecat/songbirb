a4d93a6a357fdd43cf52ae4c48c9bf79422ac918
frapplecat <nigel1138@hotmail.com>
2022-10-09 19:01:38 +0000

    begin to add js to handle letter entry

--- js/style.js
+++ js/style.js
@@ -1,6 +1,8 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
 
+const songBirb = 'ROBIN';
+
 const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '±', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<'];
 
 const guessRows = [
@@ -11,6 +13,9 @@ const guessRows = [
     ['', '', '', '', ''],
     ['', '', '', '', '']
 ];
+
+let currentRow = 0;
+let currentTile = 0;
 
 guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div');
@@ -24,14 +29,35 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     tileDisplay.append(rowElement);
 });
 
-const handleClick = () => {
-    console.log('clicked')
-};
-
 keys.forEach(key =>{
   const buttonElement = document.createElement('button');
   buttonElement.textContent = key;
   buttonElement.setAttribute('id', key);
-  buttonElement.addEventListener('click', handleClick);
+  buttonElement.addEventListener('click', () => handleClick(key));
   keyboard.append(buttonElement);
 });
+
+const handleClick = (letter) => {
+    console.log('clicked', letter);
+    if (letter === '<<') {
+        console.log('delete letter');
+        return;
+    }
+    if (letter === '±') {
+        console.log('check row');
+        return;
+    }
+    addLetter (letter);
+};
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile);
+        tile.textContent = letter;
+        guessRows[currentRow][currentTile] = letter;
+        tile.setAttribute('data', letter);
+        currentTile++;
+        console.log('guessRows', guessRows);
+    }
+ }
+
