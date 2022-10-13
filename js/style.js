fdafc1129e7d780015c82b4bf0c06c6ad9f7df5d
frapplecat <nigel1138@hotmail.com>
2022-10-13 14:04:08 +0000

    correct centering on tile rows and keyboard

--- js/style.js
+++ js/style.js
@@ -53,14 +53,14 @@ const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', '
 
 // AUDIO BUTTON
 
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const buttons = document.querySelectorAll("button");
+// const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+// const buttons = document.querySelectorAll("button");
 
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    audio.play();
-  });
-});
+// buttons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     audio.play();
+//   });
+// });
 
 
 
